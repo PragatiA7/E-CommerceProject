a42031c5e3e3f61e92a6afb3f9d57db53371b687
PragatiA7 <pragatiasatkar24@gmail.com>
2025-03-24 17:07:05 +0530

    First Commit

---
+++ src/test/java/com/lcwd/electronic/storef/ElectronicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwd.electronic.storef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
